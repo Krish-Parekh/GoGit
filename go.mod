a2f252b8d95f04439f3ff4e6ec38da63feb5641b
KrishParekh <krishparekh303@gmail.com>
2025-07-31 18:19:04 +0530

    feat: code for 'init' command, and creating directories and subdirectories

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Krish-Parekh/GoGit
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func initGitDir(baseDir string) error {
+	// Create .git directory and subdirectories
+	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+		fullPath := baseDir + string(os.PathSeparator) + dir
+		if err := os.MkdirAll(fullPath, 0755); err != nil {
+			log.Printf("error creating directory: %v", err)
+			return fmt.Errorf("error creating directory: %w", err)
+		}
+	}
+	// Create HEAD file
+	headFileContents := []byte("ref: refs/heads/main\n")
+	headPath := baseDir + string(os.PathSeparator) + ".git" + string(os.PathSeparator) + "HEAD"
+	if err := os.WriteFile(headPath, headFileContents, 0644); err != nil {
+		log.Printf("error writing file: %v", err)
+		return fmt.Errorf("error writing file: %w", err)
+	}
+	return nil
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	if len(os.Args) < 2 {
+		log.Fatalf("usage: gogit <command> [<args>...]")
+	}
+
+	switch command := os.Args[1]; command {
+	case "init":
+		if err := initGitDir("."); err != nil {
+			log.Fatalf("init failed: %v", err)
+		}
+		log.Println("initialized git directory")
+	case "cat-file":
+	default:
+		fmt.Println("command: ", command)
+		log.Fatalf("unknown command: %s", command)
+	}
+}
