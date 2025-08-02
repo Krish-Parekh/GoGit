207a93800bfe4ab4ef3cf8bd0b0b623e091939c1
KrishParekh <krishparekh303@gmail.com>
2025-08-02 11:25:55 +0530

    chore: added comments to understand ReadObject function

--- internal/object/reader.go
+++ internal/object/reader.go
@@ -11,6 +11,26 @@ import (
 	"strings"
 )
 
+/*
+The hash comes from the sha1 of the content. If you take the zlib compressed data and pipe it through sha1sum, you get the filename.
+
+Hash is done using SHA1SUM algorithm, for which the output size is 20 bytes (160 bits/40 hex characters).
+The first 2 characters of the hash are used to create the folder name.
+The remaining 38 characters are used to create the file name.
+
+The objects are stored in the .git/objects folder.
+for example, if hash is f713b3c87b42cd63f791a27aff9743ea990f89fb, then the object is stored in the .git/objects/f7/13b3c87b42cd63f791a27aff9743ea990f89fb folder.
+
+
+Git makes use of zlib to compress the objects before storing them (https://git-scm.com/book/en/v2/Git-Internals-Git-Objects)
+
+For us to read the object, we need to decompress the data and then read the header and the content.
+Structure of the object:
+<object type> <space> <content length> <null byte> <content>
+Example:
+blob 10\0Hello World
+*/
+
 func ReadObject(hash string) (string, []byte, error) {
 	if len(hash) != 40 {
 		return "", nil, fmt.Errorf("invalid hash length: %s", hash)
